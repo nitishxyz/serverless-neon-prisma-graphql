c810fe97d7c7223a35fc10e71c39a31c9b9f9ceb
Nitish Sharma <nitishxyz@proton.me>
2023-12-20 22:24:49 +0000

    working code with prisma and apollo over aws lambda

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { ApolloServer } from "@apollo/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 // import schema from "./prisma/schema.prisma";
 // import x from "../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node";
 
@@ -13,13 +16,63 @@ import {
 
 const typeDefs = `#graphql
   type Query {
-    hello: String
+    fetchUsers: [User]
+  }
+
+  type User {
+    id: Int!
+    name: String!
+    email: String!
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int!
+    title: String!
+    content: String!
+    published: Boolean!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
+    createDraft(title: String!, content: String!, authorEmail: String!): Post!
+    publish(id: Int!): Post
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    fetchUsers: async () => {
+      const users = await prisma.user.findMany({
+        include: {
+          posts: true,
+        },
+      });
+      return users;
+    },
+  },
+  Mutation: {
+    // @ts-expect-error
+    createUser: async (parent, args) => {
+      const user = await prisma.user.create({
+        data: {
+          name: args.name,
+          email: args.email,
+        },
+      });
+      return user;
+    },
+    // @ts-expect-error
+    createDraft: async (parent, args) => {
+      const post = await prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+          published: false,
+        },
+      });
+      return post;
+    },
   },
 };
 

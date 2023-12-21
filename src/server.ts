35fdd38af901f2071f43e31f829262c8d04bd110
Nitish Sharma <nitishxyz@proton.me>
2023-12-21 14:56:47 +0530

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,6 @@ import { ApolloServer } from "@apollo/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-// import schema from "./prisma/schema.prisma";
-// import x from "../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node";
-
-// if (process.env.NODE_ENV !== "production") {
-//   console.debug(schema, x);
-// }
 
 import {
   startServerAndCreateLambdaHandler,
@@ -81,10 +75,7 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// This final export is important!
-
 export const graphqlHandler = startServerAndCreateLambdaHandler(
   server,
-  // We will be using the Proxy V2 handler
   handlers.createAPIGatewayProxyEventV2RequestHandler()
 );
